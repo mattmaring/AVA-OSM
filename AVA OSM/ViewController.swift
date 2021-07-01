9218af9c56bd26ae9a715a72f092e4aa6849a9fa
Matthew Maring <matthewhmaring@gmail.com>
2021-07-01 08:56:45 -0400

    Initial Commit

---
+++ AVA OSM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVA OSM
+//
+//  Created by Matt Maring on 7/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
